5cd0eb43d0628f740550173e4f90002dfb857f86
Thibaut Mottet <thibaut.mottet@gmail.com>
2015-12-26 21:16:28 +0100

    First init

---
+++ README.md
@@ -0,0 +1,45 @@
+# generator-jhipster-spring-social-connectors
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Add Material design to your JHipster application
+
+## Usage
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be use in a JHipster application.
+
+![](/static/generator-jhipster-spring-social-connectors-install.gif)
+
+![](/static/generator-jhipster-spring-social-connectors-demo.gif)
+
+## Installation
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed.
+
+```bash
+npm install -g generator-jhipster-spring-social-connectors
+```
+
+Then run the module on a JHipster generated application:
+
+```bash
+yo jhipster-spring-social-connectors
+```
+
+## Bootstrap Material Design  
+
+Material Design for Bootstrap is a theme for Bootstrap 3 which lets you use the new Google Material Design in your favorite front-end framework.
+
+- Github: https://github.com/FezVrasta/spring-social-connectors
+- Website: http://fezvrasta.github.io/spring-social-connectors/
+
+
+## License
+
+Apache-2.0 © Thibaut Mottet
+
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-spring-social-connectors.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-spring-social-connectors
+[daviddm-image]: https://david-dm.org/moifort/generator-jhipster-spring-social-connectors.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/moifort/generator-jhipster-spring-social-connectors
+[travis-image]: https://travis-ci.org/moifort/generator-jhipster-spring-social-connectors.svg?branch=master
+[travis-url]: https://travis-ci.org/moifort/generator-jhipster-spring-social-connectors
---
+++ generators/app/index.js
@@ -0,0 +1,96 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var fs = require('fs');
+
+// Stores JHipster variables
+var jhipsterVar = {moduleName: 'spring-social-connectors'};
+
+// Stores JHipster functions
+var jhipsterFunc = {};
+
+module.exports = yeoman.generators.Base.extend({
+
+    templates: function () {
+        this.composeWith('jhipster:modules', {
+            options: {
+                jhipsterVar: jhipsterVar, jhipsterFunc: jhipsterFunc
+            }
+        });
+    },
+
+    prompting: function () {
+        var done = this.async();
+
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the ' + chalk.red('JHipster Spring social connectors') + ' generator!'
+        ));
+
+        var prompts = [
+            {
+                type: 'confirm',
+                name: 'installDesign',
+                message: 'Do you want to install Bootstrap Material design?',
+                default: false
+            }
+        ];
+
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
+
+            done();
+        }.bind(this));
+    },
+
+    writing: function () {
+        var done = this.async();
+
+        this.baseName = jhipsterVar.baseName;
+        this.packageName = jhipsterVar.packageName;
+        this.angularAppName = jhipsterVar.angularAppName;
+        this.frontendBuilder = jhipsterVar.frontendBuilder;
+
+        this.installDesign = this.props.installDesign;
+
+        if (!this.installDesign) {
+            return;
+        }
+
+        jhipsterFunc.addBowerDependency('bootstrap', '3.3.6');
+        jhipsterFunc.addBowerDependency('arrive', '2.3.0');
+        jhipsterFunc.addBowerDependency('spring-social-connectors', '0.5.6');
+
+        var config = "$.material.init();";
+        jhipsterFunc.addAngularJsConfig([''], config, 'Initialize material design');
+
+        var navbarFullPath = 'src/main/webapp/scripts/components/navbar/navbar.html';
+        var file = fs.readFileSync(navbarFullPath, 'utf8');
+        file = file.replace(/class="dropdown pointer"/g, 'dropdown');
+        file = file.replace(/class="dropdown-toggle" data-toggle="dropdown"/g, 'dropdown-toggle');
+        fs.writeFileSync(navbarFullPath, file);
+
+        done();
+    },
+
+    install: function () {
+        var injectDependenciesAndConstants = function () {
+            switch (this.frontendBuilder) {
+                case 'gulp':
+                    this.spawnCommand('gulp', ['ngconstant:dev', 'wiredep:test', 'wiredep:app']);
+                    break;
+                case 'grunt':
+                default:
+                    this.spawnCommand('grunt', ['ngconstant:dev', 'wiredep']);
+            }
+        };
+
+        this.installDependencies({
+                callback: injectDependenciesAndConstants.bind(this)
+            }
+        );
+    }
+});
+
